9bf6011eb8351a368b76dd1ad8157ac9553dd4f6
Trang Le <lethutrang101@gmail.com>
2022-06-14 11:49:05 +0700

    feat: add padding top to Container component

--- components/Container.js
+++ components/Container.js
@@ -1,12 +1,14 @@
 const Container = ({ children, fullWidth }) => {
   return (
-    <main
-      className={`m-auto flex-grow w-full transition-all ${
-        fullWidth ? 'px-4 md:px-24' : 'max-w-2xl px-4'
-      }`}
-    >
-      {children}
-    </main>
+    <div className="wrapper pt-8 lg:pt-10">
+      <main
+        className={`m-auto flex-grow w-full transition-all ${
+          fullWidth ? 'px-4 md:px-24' : 'max-w-2xl px-4'
+        }`}
+      >
+        {children}
+      </main>
+    </div>
   )
 }
 
