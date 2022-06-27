5db1c611f06992e1f7e5e80d6e25ae9abbbed32b
Trang Le <lethutrang101@gmail.com>
2022-06-27 15:50:23 +0700

    feat(config): add user defined configs

---
+++ site.config.ts
@@ -0,0 +1,8 @@
+import siteConfig from './lib/site-config'
+
+export default siteConfig({
+  name: 'Next.js Notion Starter Kit',
+  domain: 'bytrangle.vercel.app',
+  author: 'Trang Le',
+  description: 'Example for building a non-trivial Next.js website with content sourced from Notion databases'
+})
