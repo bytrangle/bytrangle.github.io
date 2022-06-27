03efd38f99a849b759bdfda0d278a0f089d0f3db
Trang Le <lethutrang101@gmail.com>
2022-06-27 16:32:17 +0700

    feat(lib): retrieve important config prop and export site

--- lib/config.ts
+++ lib/config.ts
@@ -5,7 +5,21 @@
  * for optional depenencies.
  */
 
+import { getSiteConfig } from './get-config-value'
+import { Site } from './types'
+
 export const environment = process.env.NODE_ENV || 'development'
 export const isDev = environment === 'development'
 
+// general site config
+export const name: string = getSiteConfig('name')
+export const domain: string = getSiteConfig('domain')
+export const description: string = getSiteConfig('description', 'A non-trivial blog built with Next.js and Notion')
+export const language: string = getSiteConfig('language', 'en')
+
+export const site: Site = {
+  domain,
+  name,
+  description
+}
 // export const includeNotionIdInUrls: boolean
