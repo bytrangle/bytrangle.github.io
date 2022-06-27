0f40e32347cba27310e8ab662ededa94f9cb722a
Trang Le <lethutrang101@gmail.com>
2022-06-27 17:20:47 +0700

    feat(lib): add interface for PageError and PageProps

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,17 @@
+import { ExtendedRecordMap } from 'notion-types'
+
+export interface PageError {
+  message?: string
+  statusCode: number
+}
+
+export interface PageProps {
+  site?: Site
+  recordMap?: ExtendedRecordMap
+  pageId?: string
+  error?: PageError
+}
+
 export interface Site {
   name: string
   domain: string
