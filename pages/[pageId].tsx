a36263881737202487d4e347a21eaa576eed1ee9
Trang Le <lethutrang101@gmail.com>
2022-06-28 11:57:16 +0700

    feat: rewirte dynamic path

---
+++ pages/[pageId].tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { ExtendedRecordMap } from 'notion-types'
+import notion from 'lib/notion-api'
+import { isDev, domain } from '../lib/config'
+import getCanonicalPageMap from 'lib/get-canonical-page-map'
+import resolveNotionPage from 'lib/resolve-notion-page'
+import NotionPage from '../components/NotionPage'
+
+const rootNotionPageId = process.env.NOTION_DATABASE_ID
+
+export const getStaticProps = async (context) => {
+  const rawPageId = (context.params.pageId as string)
+  console.log({ rawPageId })
+  const recordMap = await notion.getPage(rawPageId)
+  const props = await resolveNotionPage(domain, rawPageId)
+  console.log({ props })
+  return {
+    props: {
+      recordMap
+    },
+    revalidate: 10
+  }
+}
+export async function getStaticPaths() {
+  const canonicalPageMap= await getCanonicalPageMap()
+  const staticPaths = {
+    paths: Object.keys(canonicalPageMap).map((pageId) => ({
+      params: {
+        pageId
+      }
+    })),
+    fallback: true
+  }
+  // console.log(staticPaths.paths)
+  if (isDev) {
+    return {
+      paths: [],
+      fallback: true
+    }
+  }
+  return staticPaths
+}
+
+export default function Page({ recordMap }: { recordMap: ExtendedRecordMap }) {
+  return <NotionPage recordMap={recordMap} rootPageId={rootNotionPageId} />
+}
