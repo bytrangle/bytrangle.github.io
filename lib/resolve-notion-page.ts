7213c00b71cd6c865cef14f39b510076890b5973
Trang Le <lethutrang101@gmail.com>
2022-06-28 12:19:42 +0700

    feat(lib): return error object in case of error

--- lib/resolve-notion-page.ts
+++ lib/resolve-notion-page.ts
@@ -1,5 +1,6 @@
 import { parsePageId } from 'notion-utils'
 import { ExtendedRecordMap } from 'notion-types'
+import handlePageError from './handle-page-error'
 import { site } from './config'
 import notion from './notion-api'
 
@@ -18,5 +19,5 @@ export default async function resolveNotionPage(domain: string, rawPageId?: stri
     }
   }
   const props = { site, recordMap, pageId }
-  return props
+  return { ...props, ...(await handlePageError(props)) }
 }
