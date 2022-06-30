32c97535cd7f8abd7440966adebab7211956c97d
Trang Le <lethutrang101@gmail.com>
2022-06-30 11:36:18 +0700

    feat: add syntax highlighting for code component

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,12 +1,58 @@
 import * as React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
 
 import { NotionRenderer } from 'react-notion-x'
 import { ExtendedRecordMap } from 'notion-types'
 import { getPageTitle } from 'notion-utils'
 import * as types from 'lib/types'
 import mapPageUrl from 'lib/map-page-url'
+
+// -----------------------------------------------------------------------------
+// dynamic imports for optional components
+// -----------------------------------------------------------------------------
+
+const Code = dynamic(() =>
+  import('react-notion-x/build/third-party/code').then(async (m) => {
+    await Promise.all([
+      import('prismjs/components/prism-markup-templating.js'),
+      import('prismjs/components/prism-markup.js'),
+      import('prismjs/components/prism-bash.js'),
+      import('prismjs/components/prism-c.js'),
+      import('prismjs/components/prism-cpp.js'),
+      import('prismjs/components/prism-csharp.js'),
+      import('prismjs/components/prism-docker.js'),
+      import('prismjs/components/prism-java.js'),
+      import('prismjs/components/prism-js-templates.js'),
+      import('prismjs/components/prism-coffeescript.js'),
+      import('prismjs/components/prism-diff.js'),
+      import('prismjs/components/prism-git.js'),
+      import('prismjs/components/prism-go.js'),
+      import('prismjs/components/prism-graphql.js'),
+      import('prismjs/components/prism-handlebars.js'),
+      import('prismjs/components/prism-less.js'),
+      import('prismjs/components/prism-makefile.js'),
+      import('prismjs/components/prism-markdown.js'),
+      import('prismjs/components/prism-objectivec.js'),
+      import('prismjs/components/prism-ocaml.js'),
+      import('prismjs/components/prism-python.js'),
+      import('prismjs/components/prism-reason.js'),
+      import('prismjs/components/prism-rust.js'),
+      import('prismjs/components/prism-sass.js'),
+      import('prismjs/components/prism-scss.js'),
+      import('prismjs/components/prism-solidity.js'),
+      import('prismjs/components/prism-sql.js'),
+      import('prismjs/components/prism-stylus.js'),
+      import('prismjs/components/prism-swift.js'),
+      import('prismjs/components/prism-wasm.js'),
+      import('prismjs/components/prism-yaml.js')
+    ])
+    return m.Code
+  })
+)
 
 const NotionPage: React.FC<types.PageProps> = ({
   site,
@@ -15,6 +61,15 @@ const NotionPage: React.FC<types.PageProps> = ({
   pageId
 }) => {
   const router = useRouter()
+
+  const components = React.useMemo(
+    () => ({
+      nextImage: Image,
+      nextLink: Link,
+      Code
+    }),
+    []
+  )
   const siteMapPageUrl = React.useMemo(() => {
     return mapPageUrl(site, recordMap)
   }, [site, recordMap])
@@ -30,6 +85,7 @@ const NotionPage: React.FC<types.PageProps> = ({
         <meta name='description' content='React Notion X minimal demo' />
       </Head>
       <NotionRenderer
+        components={components}
         recordMap={recordMap}
         darkMode={false}
         mapPageUrl={siteMapPageUrl}
