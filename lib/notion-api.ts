ede93fabf7a8f2f8dc2aaa2ebb19b2c4c9e09682
Trang Le <lethutrang101@gmail.com>
2022-07-07 15:02:54 +0700

    feat(lib): add official Notion client

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,5 +1,10 @@
 import { NotionAPI } from 'notion-client'
+import { Client } from '@notionhq/client'
 
 const notion = new NotionAPI()
+
+export const officialNotionClient = new Client({
+  auth: process.env.NOTION_SECRET
+})
 
 export default notion
