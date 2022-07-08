59619461c0f7bbd18db1293ce59edaf455037afe
Trang Le <lethutrang101@gmail.com>
2022-07-08 11:01:04 +0700

    feat(lib): get the first child block of each article

--- lib/get-blog-feed.ts
+++ lib/get-blog-feed.ts
@@ -1,4 +1,4 @@
-import notion, { officialNotionClient } from './notion-api'
+import { officialNotionClient } from './notion-api'
 import getDatabasePageTitle from './get-database-page-title'
 
 export default async function getBlogFeed(count = 3) {
@@ -22,9 +22,25 @@ export default async function getBlogFeed(count = 3) {
     }
   })
   const posts = response["results"].slice(0, count)
-  const blogFeed = posts.map(page => {
+  const blogFeed = await Promise.all(posts.map(async (page) => {
+    const { id } = page
     const title = getDatabasePageTitle(page)
-    return {title}
-  })
+    const richText = page["properties"]["Description"]["rich_text"]
+    console.log({ richText })
+    let descriptionText = ""
+    if (richText.length !== 0) {
+      descriptionText = richText.map(elem => elem["plain_text"]).join("")
+    } else {
+      const response = await officialNotionClient.blocks.children.list({
+        block_id: id,
+        page_size: 1
+      })
+      const firstBlock = response.results[0]
+      const firstBlockType = firstBlock["type"]
+      const richTextArray = firstBlock[firstBlockType]["rich_text"].map(element => element["plain_text"])
+      descriptionText = richTextArray.join("")
+    }
+    return { id, title, description: descriptionText }
+  }))
   return blogFeed || []
 }
