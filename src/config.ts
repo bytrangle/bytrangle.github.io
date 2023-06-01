14c6614c2e0d95f93b5c3b3dbcff3f6cfdd75c7f
Trang Le <trang.thule@zoho.com>
2023-06-01 17:42:28 +0700

    Change default values

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://astro-paper.pages.dev/",
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Trang Le",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
